c6412c4445c2fa8c149d6c5c2227c2f8b0e555dc
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-06-10 10:16:25 -0300

    Changes Modificado o TarefaService para usar ResponseEntity, para nao seixar o usuario entrar com tarefas com campos nulos ou em branco, adicionado tambem a dependencia validation.

--- src/main/java/br/phsx/todoList/controller/TarefaController.java
+++ src/main/java/br/phsx/todoList/controller/TarefaController.java
@@ -1,8 +1,14 @@
 package br.phsx.todoList.controller;
 
 import br.phsx.todoList.entity.Tarefa;
-import org.springframework.web.bind.annotation.*;
 import br.phsx.todoList.service.TarefaService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import br.phsx.todoList.dto.TarefaDTO;
 
 import java.util.List;
 
@@ -17,8 +23,12 @@ public class TarefaController {
     }
 
     @PostMapping("/")
-    public Tarefa adicionarTarefa(@RequestBody Tarefa tarefa){
-        return tarefaService.adicionarTarefa(tarefa);
+    public ResponseEntity<Tarefa> criarTarefa(@Valid @RequestBody TarefaDTO tarefaDTO){
+        Tarefa novaTarefa = new Tarefa();
+        novaTarefa.setDescricao(tarefaDTO.getDescricao());
+        novaTarefa.setDataPrazo(tarefaDTO.getDataPrazo());
+        Tarefa tarefaSalva = tarefaService.adicionarTarefa(novaTarefa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaTarefa);
     }
 
     @GetMapping("/")
--- src/main/java/br/phsx/todoList/entity/Tarefa.java
+++ src/main/java/br/phsx/todoList/entity/Tarefa.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -11,6 +14,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -19,9 +24,55 @@ public class Tarefa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Esse campo não pode estar em branco.")
     private String descricao;
+
+    @NotNull(message = "A data do prazo não pode ser nula.")
+    @FutureOrPresent(message = "A data do prazo não pode ser no passado.")
     private LocalDate dataPrazo;
+
     private boolean concluido;
+
     @CreationTimestamp
     private LocalDateTime dataCriacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataPrazo() {
+        return dataPrazo;
+    }
+
+    public void setDataPrazo(LocalDate dataPrazo) {
+        this.dataPrazo = dataPrazo;
+    }
+
+    public boolean isConcluido() {
+        return concluido;
+    }
+
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
